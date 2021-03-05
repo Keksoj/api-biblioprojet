19bdaf2b8fd04313974826c36b35aff61f444416
Emmanuel Bosquet <bjokac@gmail.com>
2021-03-05 09:56:55 +0100

    added swagger.json

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace BiblioApi
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notre API Biblioprojet");
             });
 
             app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
